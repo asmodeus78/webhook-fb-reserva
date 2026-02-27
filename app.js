578b360cc69d9d2a203b372b3e182962f62fe4db
asmodeus78 <asmodeus78@gmail.com>
2026-02-27 16:35:43 +0100

    Comment out console log and response in POST route
    Comment out logging and response for webhook.

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
-  console.log(JSON.stringify(req.body, null, 2));
-  res.status(200).end();
+  //console.log(JSON.stringify(req.body, null, 2));
+  //res.status(200).end();
 
   const body = req.body;
   if (body.object === 'whatsapp_business_account'){
